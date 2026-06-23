6149e51a120a9780ab4bbbbf61e55fb9ef317aeb
inaciocr04 <inaciocr04@gmail.com>
2026-06-23 11:50:52 +0200

    fix: formater les jest.config.js avec Prettier

--- services/notification-service/jest.config.js
+++ services/notification-service/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     transform: {
-        '^.+\\.ts$': ['ts-jest', { diagnostics: false, tsconfig: 'tsconfig.json' }],
+        '^.+\\.ts$': [
+            'ts-jest',
+            { diagnostics: false, tsconfig: 'tsconfig.json' },
+        ],
     },
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
     testMatch: ['<rootDir>/spec/**/*.spec.ts', '<rootDir>/spec/**/*.test.ts'],
